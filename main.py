3f006160af9dbd66c4b3393ef2ac4acb2ffbbf62
107124 <kredfox4@gmail.com>
2021-03-24 16:35:21 -0400

    added in some sound effects for throwing snowballs and getting hit

--- main.py
+++ main.py
@@ -3,15 +3,18 @@ import os
 
 #have a font for the text in the game
 pygame.font.init()
+# load sound effects library
+pygame.mixer.init()
 
 # setting the width and height as constants to the size of the game window
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
 # set the game display name
-pygame.display.set_caption(("Harry Potter Duels"))
+pygame.display.set_caption(("The Office Snowball Fight"))
 
-HEALTH_TEXT = pygame.font.SysFont("comicsans", 55)
+HEALTH_TEXT = pygame.font.SysFont("roboto", 55)
+WINNER_TEXT = pygame.font.SysFont("roboto", 55)
 
 # predefine a color for the game
 # pygame uses RGB and takes it as a tuple:
@@ -19,6 +22,8 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
+
+
 # Setting a border for the players to not be able to cross (Rect = rectangle)
 # WIDTH/2 - 5 put's it right in the middle horizontally
 BORDER = pygame.Rect(WIDTH//2, 0, 10, HEIGHT)
@@ -30,26 +35,36 @@ VEL = 5
 BULLET_VEL = 7
 # Limit of bullets
 MAX_BULLETS = 3
-PLAYER_WIDTH, PLAYER_HEIGHT = 200, 150
+PLAYER_WIDTH, PLAYER_HEIGHT = 150, 150
+
+# LAST THING TO DO ON PROJECT enter sounds that are mp3 files
+# if you're doing wav, mp3 files, do pygame.mixer.Sound(then the path).
+GAME_MUSIC = pygame.mixer.Sound(os.path.join("sounds", "office.mp3"))
+INTRO_FIGHT_SOUND = pygame.mixer.Sound(os.path.join("sounds", "ready_fight.wav"))
+JIM_HIT_SOUND = pygame.mixer.Sound(os.path.join("sounds", "jim_ouch.wav"))
+BULLET_FIRE_SOUND = pygame.mixer.Sound(os.path.join("sounds", "spell_shoot.wav"))
+DWIGHT_OUCH_SOUND = pygame.mixer.Sound(os.path.join("sounds", "dwight_ouch.wav"))
+KO_SOUND = pygame.mixer.Sound(os.path.join("sounds", "KO.mp3"))
+# dwight_DIES = pygame.mixer.Sound(os.path.join("sounds", "expelled2.wav"))
 
 # EVENTS in case they get hit. It may look strange but the 1 and 2 is to make them unique, they can't both be 1.
-HARRY_HIT = pygame.USEREVENT + 1
-HERMIONE_HIT = pygame.USEREVENT + 2
+JIM_HIT = pygame.USEREVENT + 1
+DWIGHT_HIT = pygame.USEREVENT + 2
 
-HARRY_IMAGE = pygame.image.load(os.path.join("players", "lego_harry.png"))
-HARRY_IMAGE = pygame.transform.rotate(pygame.transform.scale(
-    HARRY_IMAGE, (PLAYER_WIDTH, PLAYER_HEIGHT)), 0)
+JIM_IMAGE = pygame.image.load(os.path.join("players", "jim.png"))
+JIM_IMAGE = pygame.transform.rotate(pygame.transform.scale(
+    JIM_IMAGE, (PLAYER_WIDTH, PLAYER_HEIGHT)), 0)
 
 
-HERMIONE_IMAGE = pygame.image.load(os.path.join("players", "IMG_2286.PNG"))
-HERMIONE_IMAGE = pygame.transform.rotate(pygame.transform.scale(
-    HERMIONE_IMAGE, (PLAYER_WIDTH, PLAYER_HEIGHT)), 180)
+DWIGHT_IMAGE = pygame.image.load(os.path.join("players", "dwight.PNG"))
+DWIGHT_IMAGE = pygame.transform.rotate(pygame.transform.scale(
+    DWIGHT_IMAGE, (PLAYER_WIDTH, PLAYER_HEIGHT)), 0)
 
 # scale so that the image will fit nicely
-BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join("backgrounds", "lego_grass.jpeg")), (WIDTH, HEIGHT))
+BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join("backgrounds", "office_background.png")), (WIDTH, HEIGHT))
 
 
-def draw_window(harry, hermione, harry_bullets, hermione_bullets, harry_health, hermione_health):
+def draw_window(jim, dwight, jim_bullets, dwight_bullets, jim_health, dwight_health):
 
     # NOTE DRAW things in order, otherwise they stack on top of each other
     # we set the window color.
@@ -60,132 +75,161 @@ def draw_window(harry, hermione, harry_bullets, hermione_bullets, harry_health, 
     pygame.draw.rect(WIN, BLACK, BORDER)
     # render some health stats.
 
-    harry_health_text = HEALTH_TEXT.render(f"Health: {str(harry_health)}", 1, WHITE)
-    hermione_health_text = HEALTH_TEXT.render(f"Health: {str(hermione_health)}", 1, WHITE)
+    jim_health_text = HEALTH_TEXT.render(f"Health: {str(jim_health)}", 1, WHITE)
+    dwight_health_text = HEALTH_TEXT.render(f"Health: {str(dwight_health)}", 1, WHITE)
     # 10, 10 and -10, 10 is just padding from where the text will be
-    WIN.blit(hermione_health_text, (WIDTH - hermione_health_text.get_width() - 10, 10))
-    WIN.blit(harry_health_text, (10, 10))
+    WIN.blit(dwight_health_text, (WIDTH - dwight_health_text.get_width() - 10, 10))
+    WIN.blit(jim_health_text, (10, 10))
 
     # we use the .blit() to add an image or text to the window
     # image, and then it's position on the window
-    WIN.blit(HARRY_IMAGE, (harry.x, harry.y))
-    WIN.blit(HERMIONE_IMAGE, (hermione.x, hermione.y))
+    WIN.blit(JIM_IMAGE, (jim.x, jim.y))
+    WIN.blit(DWIGHT_IMAGE, (dwight.x, dwight.y))
 
 
 
     # DRAW the bullets for each character
-    for bullet in harry_bullets:
-        pygame.draw.rect(WIN, RED, bullet)
+    for bullet in jim_bullets:
+        pygame.draw.rect(WIN, WHITE, bullet)
+        # pygame.draw.circle(WIN, WHITE, (jim.x, jim.y), 15, 15)
 
-    for bullet in hermione_bullets:
-        pygame.draw.rect(WIN, YELLOW, bullet)
+    for bullet in dwight_bullets:
+        pygame.draw.rect(WIN, WHITE, bullet)
+        # pygame.draw.circle(WIN, WHITE, (dwight.x, dwight.y), 15, 15)
 
     # The new display setting white will not update unless we do the following update code:
     pygame.display.update()
 
-def handle_harry_movement(keys_pressed, harry):
-    # moving the players around which will update it's position every time it loops
-    if keys_pressed[pygame.K_a] and harry.x - VEL > 0:  # left key AND prevent him from moving past the left screen border
-        harry.x -= VEL
-    if keys_pressed[pygame.K_d] and harry.x + VEL + harry.width < BORDER.x: # right key AND prevent him from moving past the center screen border
-        harry.x += VEL
-    if keys_pressed[pygame.K_w] and harry.y - VEL > 0:  # up key
-        harry.y -= VEL
-    if keys_pressed[pygame.K_s] and harry.y + VEL + harry.height < HEIGHT:  # down key
-        harry.y += VEL
+def draw_winner(text):
+    KO_SOUND.play()
+    draw_text = WINNER_TEXT.render(text, 1, WHITE)
+    # this will put the winner text over the center of the screen.
+    WIN.blit(draw_text, (WIDTH/2 - draw_text.get_width()/2, HEIGHT/2 - draw_text.get_height()/2))
+    # update the display if someone wins
+    pygame.display.update()
+    # this pause will display for however many miliseconds
+    pygame.time.delay(5000)
 
-def handle_hermione_movement(keys_pressed, hermione):
+def handle_jim_movement(keys_pressed, jim):
     # moving the players around which will update it's position every time it loops
-    if keys_pressed[pygame.K_LEFT] and hermione.x - VEL > BORDER.x + BORDER.width:  # left key
-        hermione.x -= VEL
-    if keys_pressed[pygame.K_RIGHT] and hermione.x + VEL + hermione.width < WIDTH:  # right key
-        hermione.x += VEL
-    if keys_pressed[pygame.K_UP] and hermione.y - VEL > 0:  # up key
-        hermione.y -= VEL
-    if keys_pressed[pygame.K_DOWN] and hermione.y + VEL + hermione.height < HEIGHT:  # down key
-        hermione.y += VEL
+    if keys_pressed[pygame.K_a] and jim.x - VEL > 0:  # left key AND prevent him from moving past the left screen border
+        jim.x -= VEL
+    if keys_pressed[pygame.K_d] and jim.x + VEL + jim.width < BORDER.x: # right key AND prevent him from moving past the center screen border
+        jim.x += VEL
+    if keys_pressed[pygame.K_w] and jim.y - VEL > 0:  # up key
+        jim.y -= VEL
+    if keys_pressed[pygame.K_s] and jim.y + VEL + jim.height < HEIGHT:  # down key
+        jim.y += VEL
 
-def handle_bullets(harry_bullets, hermione_bullets, harry, hermione):
-    for bullet in harry_bullets:
+def handle_dwight_movement(keys_pressed, dwight):
+    # moving the players around which will update it's position every time it loops
+    if keys_pressed[pygame.K_LEFT] and dwight.x - VEL > BORDER.x + BORDER.width:  # left key
+        dwight.x -= VEL
+    if keys_pressed[pygame.K_RIGHT] and dwight.x + VEL + dwight.width < WIDTH:  # right key
+        dwight.x += VEL
+    if keys_pressed[pygame.K_UP] and dwight.y - VEL > 0:  # up key
+        dwight.y -= VEL
+    if keys_pressed[pygame.K_DOWN] and dwight.y + VEL + dwight.height < HEIGHT:  # down key
+        dwight.y += VEL
+
+def handle_bullets(jim_bullets, dwight_bullets, jim, dwight):
+    for bullet in jim_bullets:
         # make the bullets go RIGHT towards the WIDTH, or right of the screen
         bullet.x += BULLET_VEL
         # if the bullet collides, then make the bullet disappear
-        if hermione.colliderect(bullet):
-            pygame.event.post(pygame.event.Event(HERMIONE_HIT))
-            harry_bullets.remove(bullet)
+        if dwight.colliderect(bullet):
+            pygame.event.post(pygame.event.Event(DWIGHT_HIT))
+            jim_bullets.remove(bullet)
+            DWIGHT_OUCH_SOUND.play()
             # elif so we don't remove it twice, this will remove the bullet if it's off the screen
             # otherwise it will stay in the list and not allow you to shoot anymore
         elif bullet.x > WIDTH:
-            harry_bullets.remove(bullet)
+            jim_bullets.remove(bullet)
 
-    for bullet in hermione_bullets:
+    for bullet in dwight_bullets:
         # make the bullets go LEFT towards 0, or the left of the screen
         bullet.x -= BULLET_VEL
         # if the bullet collides, then make the bullet disappear
-        if harry.colliderect(bullet):
-            pygame.event.post(pygame.event.Event(HARRY_HIT))
-            hermione_bullets.remove(bullet)
+        if jim.colliderect(bullet):
+            pygame.event.post(pygame.event.Event(JIM_HIT))
+            dwight_bullets.remove(bullet)
         elif bullet.x < 0:
-            hermione_bullets.remove(bullet)
+            dwight_bullets.remove(bullet)
 
 # the game will run until x on the window
 def main():
-    harry = pygame.Rect(100, 300, PLAYER_WIDTH, PLAYER_HEIGHT)
-    hermione = pygame.Rect(700, 300, PLAYER_WIDTH, PLAYER_HEIGHT)
+    jim = pygame.Rect(100, 300, PLAYER_WIDTH, PLAYER_HEIGHT)
+    dwight = pygame.Rect(700, 300, PLAYER_WIDTH, PLAYER_HEIGHT)
 
-    harry_bullets = []
-    hermione_bullets = []
+    jim_bullets = []
+    dwight_bullets = []
 
-    harry_health = 12
-    hermione_health = 12
+    jim_health = 12
+    dwight_health = 12
 
     clock = pygame.time.Clock()
     run = True
+    # This will play every time the game starts, even on a restart
+    # preset the volume and how many times it will loop and fade is the amount of time for fading at start and end
+    GAME_MUSIC.set_volume(0.1)
+    GAME_MUSIC.play(loops=10, fade_ms=3000)
+    INTRO_FIGHT_SOUND.play()
+
     while run:
+
         # The speed of the while loop:
         clock.tick(FPS)
         # if i hit x, the loop will break and end the game window
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-
+                # add the following at the VERY END so that it quits instead of restarts when they click the x
+                pygame.quit()
             if event.type == pygame.KEYDOWN:
                 # checking for the bullet shooting event button AND only doing it if they have enough bullets
-                if event.key == pygame.K_SPACE and len(harry_bullets) < MAX_BULLETS:
+                if event.key == pygame.K_SPACE and len(jim_bullets) < MAX_BULLETS:
                     # Where the bullet will come out of the character, the width of the bullet is 10 and height is 5
-                    bullet = pygame.Rect(harry.x + harry.width, harry.y + harry.height//2 - 2, 10, 5)
-                    harry_bullets.append(bullet)
-                if event.key == pygame.K_m and len(hermione_bullets) < MAX_BULLETS:
-                    bullet = pygame.Rect(hermione.x, hermione.y + hermione.height//2 - 2, 10, 5)
-                    hermione_bullets.append(bullet)
+                    bullet = pygame.Rect(jim.x + jim.width, jim.y + jim.height//2 - 2, 10, 5)
+                    jim_bullets.append(bullet)
+                    BULLET_FIRE_SOUND.play()
+                if event.key == pygame.K_m and len(dwight_bullets) < MAX_BULLETS:
+                    bullet = pygame.Rect(dwight.x, dwight.y + dwight.height//2 - 2, 10, 5)
+                    dwight_bullets.append(bullet)
+                    BULLET_FIRE_SOUND.play()
 
             # If hit, then minus a health point from the player
-            if event.type == HARRY_HIT:
-                harry_health -= 1
-            if event.type == HERMIONE_HIT:
-                hermione_health -= 1
+            if event.type == JIM_HIT:
+                jim_health -= 1
+                JIM_HIT_SOUND.play()
+            if event.type == DWIGHT_HIT:
+                dwight_health -= 1
+                DWIGHT_OUCH_SOUND.play()
+
 
         end_game_text = ""
-        if harry_health <= 0:
-            end_game_text = "Hermione Wins!"
-        if hermione_health <= 0:
-            end_game_text = "Harry Potter Wins!"
+        if jim_health <= 0:
+            end_game_text = "Dwight Wins!"
+
+        if dwight_health <= 0:
+            end_game_text = "Jim Wins!"
 
         if end_game_text != "":
-            pass # someone won the game
+            draw_winner(end_game_text)
+            break
 
 
-        print(harry_bullets, hermione_bullets)
-        # Movement function for harry and hermione
+        print(jim_bullets, dwight_bullets)
+        # Movement function for jim and dwight
         keys_pressed = pygame.key.get_pressed()
-        handle_harry_movement(keys_pressed, harry)
-        handle_hermione_movement(keys_pressed, hermione)
+        handle_jim_movement(keys_pressed, jim)
+        handle_dwight_movement(keys_pressed, dwight)
 
-        handle_bullets(harry_bullets, hermione_bullets, harry, hermione)
+        handle_bullets(jim_bullets, dwight_bullets, jim, dwight)
 
-        draw_window(harry, hermione, harry_bullets, hermione_bullets, harry_health, hermione_health)
+        draw_window(jim, dwight, jim_bullets, dwight_bullets, jim_health, dwight_health)
 
-    pygame.quit()
+    # have this here until the very end to just restart instead of quit: pygame.quit()
+    main()
 
 # if the actual "name" of the file is "main" then it will run
 # instead of this running if we're running something else in
